76a8a6f5e023c3060507a71eb4d83b3781cf4f83
Lilia <geertszelia@gmail.com>
2016-10-06 16:40:54 +0200

    Modif du code pour factoriser le plus possible

--- index.js
+++ index.js
@@ -6,26 +6,21 @@ function main(){
 
 	function clickValider(){
 		var input = $("input").val();
-		if(partieGagnee()){
-			alert("Gagné !");
-			reset();
+		if(input == mystere){
+			annonce("Gagné !");			
 		} else if(partiePerdue()){
-			alert("Perdu");
-			reset();
-		} else if(input > mystere){
+			annonce("Perdu");
+		} else if(tentatives == 0){
 			alert("Votre nombre est trop grand");
 			diminution();
 		} else{
 			alert("Votre nombre est trop petit");
 			diminution();
 		}
-
-		function partieGagnee(){
-			return(input == mystere);			
-		}
-
-		function partiePerdue(){
-			return(tentatives == 0);
+		
+		function annonce(message){
+			alert(message);
+			reset();
 		}
 
 		function diminution(){
@@ -37,14 +32,16 @@ function main(){
 	function reset(){
 		tentatives = parseInt(prompt("Combien de tentatives voulez vous ?"), 10);
 		$("#tent").html(tentatives);
+		
 		var min = parseInt(prompt("Choisissez un nombre minimum"), 10);
 		$("#minimum").html(min);
+		
 		var max = parseInt(prompt("et maximum"), 10);
 		$("#maximum").html(max);
+		
 		alert("Devinez le nombre mystère");
 		mystere = Math.round(Math.random() * (max - min) + min);
 		console.log(mystere);
-		debugger;
 	}
 
 	$("#go").click(reset);
